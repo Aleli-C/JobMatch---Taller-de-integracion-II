37713a8f9935c22d3ef12cbaf849e991d80b2670
PINILLA MARTÍNEZ ÁLVARO FELIPE <apinilla2024@alu.uct.cl>
2025-09-27 17:04:40 -0300

    Agregar conexion a vista foro en header
    Se añade la conexion de la vista foro en el header y se arregla el problema de que se duplica

--- App/forum/page.tsx
+++ App/forum/page.tsx
@@ -1,13 +1,9 @@
-import Header from '../../components/header';
 import Forum from '../../components/Forum';
 
 export default function ForumPage() {
   return (
-    <>
-      <Header />
-      <main>
-        <Forum />
-      </main>
-    </>
+    <main>
+      <Forum />
+    </main>
   );
 }
