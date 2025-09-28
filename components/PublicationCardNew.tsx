692ea865122304d5c6f436af1cd9952bf5a6dcf0
PINILLA MARTÍNEZ ÁLVARO FELIPE <apinilla2024@alu.uct.cl>
2025-09-28 18:31:13 -0300

    Crear Componente PublicationCardNew
    Se crea el componente el cual es el componente que se usará para el formulario al momento de querer crear una publicación

---
+++ components/PublicationCardNew.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PublicarTrabajoForm from "./PublicarTrabajoForm";
+
+const PublicationCardNew: React.FC = () => {
+  return (
+    <div className="bg-white rounded-xl shadow-lg p-8 max-w-3xl mx-auto my-8 border border-gray-200">
+      <h2 className="text-2xl font-bold text-[#0069C0] mb-6 text-center">
+        Crear Una Nueva Publicación
+      </h2>
+      <PublicarTrabajoForm />
+    </div>
+  );
+};
+
+export default PublicationCardNew;
