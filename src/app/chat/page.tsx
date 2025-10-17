3ac8ae77d139aab767a8e7f329c3b4fd4f135628
gustavovlnz <gvalenzuela2023@alu.uct.cl>
2025-10-17 19:50:05 -0300

    componente ChatSidebar modificado
    se añadio mas datos de prueba, se adaptaron los campos de acuerdo a bd y se arregla el color

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -4,11 +4,10 @@ import ChatSidebar from "../../components/ChatSideBar";
 
 export default function ChatPage() {
   return (
-    <div className="flex h-screen bg-gray-100 dark:bg-gray-900">
-      <ChatSidebar />
-      <main className="flex-1 flex items-center justify-center">
-        <p className="text-gray-500">Selecciona un chat para abrirlo (aún no implementado).</p>
-      </main>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-6">
+      <div className="w-full max-w-6xl h-[85vh] shadow-sm rounded-2xl overflow-hidden bg-white">
+        <ChatSidebar />
+      </div>
     </div>
   );
 }
